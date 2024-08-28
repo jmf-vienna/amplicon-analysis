2a75ddffc9d5f2a656e98314dfcda474e3571e01
Bela <75233+sci@users.noreply.github.com>
2024-08-28 10:17:53 +0200

    clean up se row subsetting

--- R/se_filter.R
+++ R/se_filter.R
@@ -1,15 +1,15 @@
 keep_desirables <- function(se, config) {
   se |>
     SummarizedExperiment::subset(
-      SummarizedExperiment::rowData(se)$Domain %in% config[["Domain"]]
+      Domain %in% config[["Domain"]]
     )
 }
 
 filter_undesirables <- function(se, config) {
   se |>
     SummarizedExperiment::subset(!(
-      SummarizedExperiment::rowData(se)$Order %in% config[["Order"]] |
-        SummarizedExperiment::rowData(se)$Family %in% config[["Family"]] |
-        SummarizedExperiment::rowData(se)$Sequence_ID %in% config[["Sequence_ID"]]
+      Order %in% config[["Order"]] |
+        Family %in% config[["Family"]] |
+        Sequence_ID %in% config[["Sequence_ID"]]
     ))
 }
