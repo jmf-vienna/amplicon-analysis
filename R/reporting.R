e47c7edc99ef83bd0bebfd7806bf3aee1e28e042
Bela <75233+sci@users.noreply.github.com>
2024-09-09 10:36:52 +0200

    report versions and citations

--- R/reporting.R
+++ R/reporting.R
@@ -3,12 +3,39 @@ make_summary_report <- function(provenance, pipeline_version, stats) {
     provenance |>
     unlist() |>
     stringr::str_flatten(" ")
+
+  citations <-
+    c(
+      "targets", "tidyverse",
+      "SummarizedExperiment", "SingleCellExperiment", "mia",
+      "vegan", "scuttle",
+      "phyloseq", "microViz"
+    ) |>
+    purrr::map(citation_text) |>
+    stringr::str_c("* ", text = _, collapse = "\n")
+
   glue::glue(
     "# {title} summary",
     "",
-    "JMF downstream [amplicon analysis](https://github.com/jmf-vienna/amplicon-analysis) pipeline (beta) v{pipeline_version}",
+    "## Software versions",
+    "",
+    "* JMF downstream [amplicon analysis](https://github.com/jmf-vienna/amplicon-analysis) pipeline (beta) v{pipeline_version}",
+    "* [mia](https://microbiome.github.io/mia/) v{packageVersion('mia')}",
+    "* [microViz](https://david-barnett.github.io/microViz/) v{packageVersion('microViz')}",
+    "",
+    "## Citations",
+    "",
+    "{citations}",
     "",
     .sep = "\n",
     .trim = FALSE
   )
+}
+
+citation_text <- function(x) {
+  withr::with_options(
+    list(width = 10000),
+    capture.output(print(citation(x), style = "text"))
+  ) |>
+    stringr::str_replace("\\*([0-9]+)\\*", "__\\1__")
 }
