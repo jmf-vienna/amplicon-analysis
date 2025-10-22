f4808e5ec7e2ee8ec4aacb8dd56bd09b0e65625f
Bela <75233+sci@users.noreply.github.com>
2025-10-22 13:04:04 +0200

    feat: support multi subsetting

--- R/subsets.R
+++ R/subsets.R
@@ -5,9 +5,18 @@ make_se_subsets <- function(se, subset) {
 
   loadNamespace(class(se))
 
-  variable <- subset |> chuck("variable")
-  values <- subset |> chuck("values")
-  name <- subset |> pluck("name", .default = str_c(variable, ": ", values |> str_c(collapse = ", ")))
+  variable <- subset |> purrr::chuck("variable")
+  values <- subset |> purrr::chuck("values")
+
+  name <- subset |> purrr::pluck("name", .default = str_c(variable, ": ", values |> str_c(collapse = ", ")))
+  print(name)
+  # in case of multiple subsetting
+  name <-
+    se |>
+    get_provenance() |>
+    purrr::pluck("subset") |>
+    c(name) |>
+    str_c(collapse = " & ")
 
   se[, se[[variable]] %in% values] |>
     update_provenance(new = list(subset = name)) |>
